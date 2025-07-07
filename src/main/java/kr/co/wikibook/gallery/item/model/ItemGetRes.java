a71c3ff8588cf4bfb320c20dfd3025db385a4243
nususu1004 <nususu1004@gmail.com>
2025-07-07 17:10:00 +0900

    20250707_gallery_BE

---
+++ src/main/java/kr/co/wikibook/gallery/item/model/ItemGetRes.java
@@ -0,0 +1,14 @@
+package kr.co.wikibook.gallery.item.model;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ItemGetRes {
+    private int id;
+    private String name;
+    private String imgPath;
+    private int price;
+    private int discountPer;
+}
